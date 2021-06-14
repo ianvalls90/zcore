1d317a7b95acfa2346c74253bfdd97f73c820487
Tony <1414927+zfbx@users.noreply.github.com>
2021-06-14 07:25:01 -0400

    notes

---
+++ server/resources/[standalone]/dpemotes/znotes.lua
@@ -0,0 +1,3 @@
+figure out if there is a memory leak 
+
+remove keybind system and replace with custom
---
+++ server/resources/[standalone]/pma-voice/znotes.lua
@@ -0,0 +1,11 @@
+fix the config variables 
+    set voice_useNativeAudio true
+    set voice_useSendingRangeOnly true maybe if it's not buggy
+    set voice_enableUi 0 ?
+    set voice_defaultCycle to a key that makes sense and wont conflict
+    set voice_enableSubmix 1
+    set voice_enableRadioAnim 
+
+    change voice_defaultRadio from left alt as that will be used by bt-target
+
+    voice_syncData 1 .. maybe?
